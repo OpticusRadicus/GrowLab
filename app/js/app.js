fb8823d3c1969d388b54c650c6cf4f2738964818
Mira Lopez <miralopez@MacBook-Pro.local>
2015-02-15 00:04:53 -0800

    all

---
+++ app/js/app.js
@@ -0,0 +1,42 @@
+'use strict';
+
+/* App Module */
+
+var phonecatApp = angular.module('phonecatApp', [
+  'ngRoute',
+  'phonecatAnimations',
+  'phonecatControllers',
+  'phonecatFilters',
+  'phonecatServices',
+ 
+]);
+
+phonecatApp.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/phones', {
+        templateUrl: 'partials/phone-list.html',
+        controller: 'PhoneListCtrl'
+      }).
+      when('/phones/:phoneId', {
+        templateUrl: 'partials/phone-detail.html',
+        controller: 'PhoneDetailCtrl'
+      }).
+        when('/pagetwo', {
+        templateUrl: 'partials/pagetwo.html'
+      }).
+          when('/homebase', { 
+        templateUrl: 'partials/homebase.html'
+      }).
+          when('/contact', {
+        templateUrl: 'partials/contact.html'
+      }).  
+      when('/cookieList', {
+        templateUrl: 'partials/cookieList.html'
+        // controller: 'CookieListCtrl'
+      }). 
+      otherwise({
+        redirectTo: '/homebase'
+      });
+  }]);
+
---
+++ app/js/controllers.js
@@ -0,0 +1,128 @@
+'use strict';
+
+/* Controllers */
+
+var phonecatControllers = angular.module('phonecatControllers', []);
+
+phonecatControllers.controller('PhoneListCtrl', ['$scope', 'Phone',
+  function($scope, Phone, homebase, pagetwo, contact) {
+    $scope.phones = Phone.query();
+    $scope.orderProp = 'name';
+  }]);
+
+
+
+
+// phonecatApp.controller('CookieListCtrl', ['$scope', 'Cookies',
+//   function($scope, Cookies) {
+//     $scope.cookies = Cookies.query();
+//     $scope.orderProp = 'name';
+//   }]);
+
+
+// phonecatControllers.controller('CookieListCtrl', function ($scope, $http) {
+//   $http.get('posts/posts.json').success(function(data) {
+//     $scope.post = data;
+//   });
+
+//   $scope.orderProp = 'age';
+// });
+
+
+
+// phonecatApp.controller('CookieListCtrl', function ($scope, $http){
+//     $scope.posts = [
+//     {
+        
+//         "id": "chocChip", 
+//         "imageUrl": "img/cookies/chocChip.jpg", 
+//         "name": "chocChip", 
+//         "snippet": "A delicous Chocolate chip cookie"
+//     }, 
+//     {
+    
+//         "id": "orio", 
+//         "imageUrl": "img/cookies/orio.jpg", 
+//         "name": "orio", 
+//         "snippet": "A delicous Orio cookie"
+//     },
+//       {
+//         "age": 1, 
+//         "id": "motorola-xoom", 
+//         "imageUrl": "img/phones/motorola-xoom.0.jpg", 
+//         "name": "MOTOROLA XOOM\u2122", 
+//         "snippet": "The Next, Next Generation\n\nExperience the future with MOTOROLA XOOM, the world's first tablet powered by Android 3.0 (Honeycomb)."
+//     }, 
+// ]
+//   });
+
+
+// phonecatApp.controllenr('CookieListCtrl', function ($scope, $http){
+//     $scope.posts = function () {
+//       $http.get('posts/posts.json').success(function(data) {
+//    $scope.posts = data;
+//    });  
+//     }
+//       });
+
+
+    
+
+
+
+
+
+
+
+// $scope.posts = [
+//     {
+        
+//         "id": "chocChip", 
+//         "imageUrl": "img/cookies/chocChip.jpg", 
+//         "name": "chocChip", 
+//         "snippet": "A delicous Chocolate chip cookie"
+//     }, 
+//     {
+    
+//         "id": "orio", 
+//         "imageUrl": "img/cookies/orio.jpg", 
+//         "name": "orio", 
+//         "snippet": "A delicous Orio cookie"
+//     },
+//       {
+//         "age": 1, 
+//         "id": "motorola-xoom", 
+//         "imageUrl": "img/phones/motorola-xoom.0.jpg", 
+//         "name": "MOTOROLA XOOM\u2122", 
+//         "snippet": "The Next, Next Generation\n\nExperience the future with MOTOROLA XOOM, the world's first tablet powered by Android 3.0 (Honeycomb)."
+//     }, 
+// ]
+// });
+
+// phonecatControllers.controller('Banner', function ($scope){
+//   $scope.post =  {name: 'Gabrial', id: '1' }
+
+//   )};
+
+
+phonecatApp.controller('ListCtrl', function ($scope) {
+  $scope.Banner = {
+    'name': 'Valentines',
+     'snippet': '...And love for everyone',
+     'imageUrl': 'img/Grow-Labweed.jpg'
+  };
+});
+
+phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams', 'Phone',
+  function($scope, $routeParams, Phone, pagetwo, homebase) {
+    $scope.phone = Phone.get({phoneId: $routeParams.phoneId}, function(phone) {
+      $scope.mainImageUrl = phone.images[0];
+    });
+
+    $scope.setImage = function(imageUrl) {
+      $scope.mainImageUrl = imageUrl;
+    }
+  }]);
+
+
+
